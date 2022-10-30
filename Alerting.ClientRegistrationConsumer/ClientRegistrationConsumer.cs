590c702cfd723aee3a7ba717955f5212a0112de1
Полторацкий Александр <pag.2010@ya.ru>
2022-10-30 08:10:25 +0300

    Добавлен сервис регистрации клиента
    +рефакторинг

---
+++ Alerting.ClientRegistrationConsumer/ClientRegistrationConsumer.cs
@@ -0,0 +1,35 @@
+﻿using Alerting.Domain;
+using Alerting.Domain.Redis;
+using MassTransit;
+using Redis.OM;
+using Redis.OM.Searching;
+using System.Threading.Tasks;
+
+namespace Alerting.ClientRegistrationConsumer
+{
+    public class ClientRegistrationConsumer : IConsumer<ClientRegistration>
+    {
+        private readonly RedisConnectionProvider _provider;
+        private readonly RedisCollection<ClientCache> _clients;
+
+        public ClientRegistrationConsumer(RedisConnectionProvider provider)
+        {
+            _provider = provider;
+            _clients =
+                (RedisCollection<ClientCache>)provider.RedisCollection<ClientCache>();
+        }
+
+        public async Task Consume(ConsumeContext<ClientRegistration> context)
+        {
+            var registeredClient = await _clients.SingleOrDefaultAsync(ca => ca.Id == context.Message.Id);
+            if (registeredClient == null)
+            {
+                await _clients.InsertAsync(new ClientCache
+                {
+                    Id = context.Message.Id,
+                    Name = context.Message.Name,
+                });
+            }
+        }
+    }
+}
---
+++ Domain/Redis/ClientCache.cs
@@ -0,0 +1,15 @@
+﻿using Redis.OM.Modeling;
+using System;
+
+namespace Alerting.Domain.Redis
+{
+    [Document(StorageType = StorageType.Json, Prefixes = new[] { "Client" })]
+    public class ClientCache
+    {
+        [RedisIdField]
+        [Indexed]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
